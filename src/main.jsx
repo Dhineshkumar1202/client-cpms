09491aa3b44d5e3b6e6d0a632dbee7946d0d6b36
Dhineshkumar1202 <dhineshtrendy12@gmail.com>
2024-11-25 20:27:52 +0530

    files modified

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,4 @@
+
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
@@ -10,4 +11,4 @@ ReactDOM.createRoot(rootElement).render(
   <AuthProvider>
     <App />
   </AuthProvider>
-);
+);<AiFillAlipayCircle />
