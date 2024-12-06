f9fd913eb4381ea6d2557135b60763c5b79742fb
Dhineshkumar1202 <dhineshtrendy12@gmail.com>
2024-12-06 17:53:58 +0530

    files modified

--- axiosConfig.js
+++ axiosConfig.js
@@ -1,15 +1,35 @@
-// axiosConfig.js (client-side)
 import axios from 'axios';
 
-// Axios request interceptor to add the Authorization header with the token
-axios.interceptors.request.use((config) => {
-  const token = localStorage.getItem('token'); // Get the token from localStorage
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`; // Add the token to the request header
-  }
-  return config;
-}, (error) => {
-  return Promise.reject(error);
+
+const axiosInstance = axios.create({
+  baseURL: 'https://appcollege-jsbz0903.b4a.run/api', 
 });
 
-export default axios;
+
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      config.headers['Authorization'] = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+
+axiosInstance.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
+      console.error("Unauthorized: Redirecting to login...");
+    
+      window.location.href = '/login';
+    }
+    return Promise.reject(error);
+  }
+);
+
+export default axiosInstance;
