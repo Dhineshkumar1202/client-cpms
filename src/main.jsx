05e3911a2a1ec9b987f03180f305e8e068d65b8e
Dhineshkumar1202 <dhineshtrendy12@gmail.com>
2024-12-17 21:25:44 +0530

    files updated

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-import { AuthProvider } from "./context/AuthContext"; // Correctly import AuthProvider
+import { AuthProvider } from "./context/AuthContext"; 
 import "./index.css";
 
 const rootElement = document.getElementById("root");
@@ -11,7 +11,7 @@ const root = ReactDOM.createRoot(rootElement);
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <AuthProvider> {/* Wrap the app in AuthProvider */}
+      <AuthProvider> 
         <App />
       </AuthProvider>
     </BrowserRouter>
