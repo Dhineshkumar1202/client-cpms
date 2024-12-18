b081682e2b505b9f95fe1ededeec4b6641306412
Dhineshkumar1202 <dhineshtrendy12@gmail.com>
2024-12-18 19:13:31 +0530

    files updated

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default {
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
   server: {
     proxy: {
       '/api': {
@@ -11,5 +13,4 @@ export default {
       },
     },
   },
-};
-
+});
