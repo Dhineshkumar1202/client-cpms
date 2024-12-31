52a72c45375b1870bc4d8ce4ddb78e83c8f7c880
Dhineshkumar1202 <dhineshtrendy12@gmail.com>
2024-12-31 21:03:29 +0530

    files updated

--- src/services/RecruitmentStatusService.js
+++ src/services/RecruitmentStatusService.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
-const BASE_URL = "https://cpmsapp-q59f2p6k.b4a.run";  
+ 
 
 export const getRecruitmentStatus = async () => {
   try {
-    const response = await axios.get(`${BASE_URL}/recruitment-status`);
+    const response = await axios.get(`https://cpmsapp-q59f2p6k.b4a.run/api/recruitment-status`);
     return response.data;
   } catch (error) {
     throw new Error("Error fetching recruitment status");
