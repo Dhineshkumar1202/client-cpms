b0226e187a795b142d27f3f83333aaac8b98bfa2
Dhineshkumar1202 <dhineshtrendy12@gmail.com>
2025-02-03 08:47:29 +0530

    files updated

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../context/AuthContext"; 
+import "../styles/login.css"
 
 const LoginForm = () => {
   const [email, setEmail] = useState("");
